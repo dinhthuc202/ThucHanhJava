99a192d16bc45162a2200a570c30a446371a72cf
dinhthuc202 <dinhthuc202@gmail.com>
2022-03-18 11:45:09 +0700

    Create_Project_BaiThucHanh1803

--- BaiThucHanh1803/src/shapes/HinhTron.java
+++ BaiThucHanh1803/src/shapes/HinhTron.java
@@ -1,40 +1,34 @@
+package shapes;
+
 import java.util.Scanner;
 public class HinhTron {
-    public static void main(String[] args) throws Exception {
-        HinhTron ht = new HinhTron();
-        ht.NhapBanKinh();
-        ht.TinhChuVi();
-        ht.inChuVi();
-        ht.TinhDienTinh();
-        ht.inDienTich();
-    }
-
+    
     final float PI = 3.14f;
     float r;
     float cv;
     float dt;
-    void NhapBanKinh()
+    Scanner sc = new Scanner(System.in);
+    public void NhapBanKinh()
     {
         System.out.print("Nhap vao ban kinh hinh tron: ");
         Scanner sc = new Scanner(System.in);
         r = sc.nextFloat();
-        sc.close();
     }
 
-    void TinhChuVi()
+    public void tinhChuVi()
     {
         cv = 2 * PI * r;
     }
 
-    void TinhDienTinh()
+    public void tinhDienTinh()
     {
         dt = PI * r * r;
     }
-    void inChuVi()
+    public void inChuVi()
     {
         System.out.print("\nChu vi hinh tron: " + cv);
     }
-    void inDienTich()
+    public void inDienTich()
     {
         System.out.print("\nDien tich hinh tron: " + dt);
     }
